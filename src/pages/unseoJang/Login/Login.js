b8b5f11b0db17b68fc4f7a53845de62ed07fd539
unseo Jang <unseojang@seoun-iui-maegbug.local>
2021-06-25 16:55:33 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/unseoJang/Login/Login.js
+++ src/pages/unseoJang/Login/Login.js
@@ -2,14 +2,42 @@ import './Login.scss';
 import React, { Component } from 'react';
 
 class Login extends Component {
+  //login handle
+  constructor() {
+    super();
+    this.state = {
+      //state 말 그대로 컴포넌트의 상태를 수정
+      idInputValue: '',
+      pwInputValue: '',
+    };
+  }
+  handleIdInput = e => {
+    //IdInout은 event의 약자 e를 인자로 받음
+    this.setState({
+      idInputValue: `${e.target.value}`,
+    });
+  };
+
+  handlePwInput = e => {
+    this.setState({
+      pwInputValue: `${e.target.value}`,
+    });
+  };
+
+  //link handle
+  goMain = () => {
+    this.props.history.push('/main-unseo');
+  };
+
   render() {
+    let title = 'westagram';
     return (
       <section className="westagram-login-wrap other">
         <main className="main-wrap">
           <div className="contetns-wrap">
             <div className="box-wrap">
               <div className="login-wrap">
-                <h1>westagram</h1>
+                <h1> {title}</h1>
                 <div className="input-wrap">
                   <form>
                     <div className="login-box">
@@ -20,6 +48,7 @@ class Login extends Component {
                             name="ID"
                             id="userId"
                             placeholder="전호번호, 사용자 이름 또는 이메일"
+                            onChange={this.handleIdInput}
                           />
                         </label>
                       </div>
@@ -30,6 +59,7 @@ class Login extends Component {
                             name="Password"
                             id="userPassWord"
                             placeholder="비밀번호"
+                            onChange={this.handlePwInput}
                           />
                         </label>
                       </div>
@@ -37,7 +67,13 @@ class Login extends Component {
                         <button
                           type="button"
                           className="btn-login"
-                          disabled="disabled"
+                          disabled={
+                            this.state.idInputValue.indexOf('@') !== -1 &&
+                            this.state.pwInputValue.length > 5
+                              ? false
+                              : true
+                          }
+                          onClick={this.goMain}
                         >
                           <span>로그인</span>
                         </button>
