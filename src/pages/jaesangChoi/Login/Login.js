204b15cf7f77d0980fd4c44d7d5340f50aacd359
dsd <sadad@naver.com>
2021-06-26 16:53:19 +0900

    [Add]: Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/jaesangChoi/Login/Login.js
+++ src/pages/jaesangChoi/Login/Login.js
@@ -3,6 +3,36 @@ import './Login.scss';
 import Login_Button from './Login_Button/Login_Button';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: 'a',
+      pw: 0,
+      idpass: 'failed',
+      pwpass: 'shit',
+    };
+  }
+
+  handleIdInput = e => {
+    this.setState({ id: e.target.value }, () => {
+      let idValue = String(this.state.id);
+
+      idValue.indexOf('@') === -1
+        ? this.setState({ idpass: 'fauled' })
+        : this.setState({ idpass: 'passed' });
+    });
+  };
+
+  handlePwInput = e => {
+    this.setState({ pw: e.target.value }, () => {
+      let pwValue = String(this.state.pw);
+
+      pwValue.length >= 5
+        ? this.setState({ pwpss: 'nice' })
+        : this.setState({ pwpass: 'shit' });
+    });
+  };
+
   render() {
     return (
       <div className="log_in_box">
@@ -10,6 +40,7 @@ class Login extends Component {
         <div className="log_in">
           <div className="id">
             <input
+              onChange={this.handleIdInput}
               className="id_input"
               type="text"
               placeholder="전화번호, 사용자 이름 또는 이메일"
@@ -17,6 +48,7 @@ class Login extends Component {
           </div>
           <div className="password">
             <input
+              onChange={this.handlePwInput}
               className="password_input"
               type="password"
               placeholder="비밀번호"
